eca665babbf2de79d2175509a40ce1741f1a0d0d
yuyuyu <hc_yuneng@aliyun.com>
2023-11-15 10:50:43 +0800

    🎉 init:init

---
+++ tasks/full/5_initialize.ts
@@ -0,0 +1,79 @@
+import { task } from 'hardhat/config';
+import { getParamPerNetwork } from '../../helpers/contracts-helpers';
+import {
+  deployLendingPoolCollateralManager,
+  deployWalletBalancerProvider,
+  deployAaveProtocolDataProvider,
+  deployWETHGateway,
+} from '../../helpers/contracts-deployments';
+import {
+  loadPoolConfig,
+  ConfigNames,
+  getWethAddress,
+  getTreasuryAddress,
+} from '../../helpers/configuration';
+import { eEthereumNetwork, ICommonConfiguration } from '../../helpers/types';
+import { waitForTx } from '../../helpers/misc-utils';
+import { initReservesByHelper, configureReservesByHelper } from '../../helpers/init-helpers';
+import { exit } from 'process';
+import {
+  getAaveProtocolDataProvider,
+  getLendingPoolAddressesProvider,
+} from '../../helpers/contracts-getters';
+import { ZERO_ADDRESS } from '../../helpers/constants';
+
+task('full:initialize-lending-pool', 'Initialize lending pool configuration.')
+  .addFlag('verify', 'Verify contracts at Etherscan')
+  // .addParam('pool', `Pool name to retrieve configuration, supported: ${Object.values(ConfigNames)}`)
+  .setAction(async ({ verify, pool = 'Agave' }, localBRE) => {
+    try {
+      await localBRE.run('set-DRE');
+      const network = <eEthereumNetwork>localBRE.network.name;
+      const poolConfig = loadPoolConfig(pool);
+      const { ReserveAssets, ReservesConfig, WNativeSymbol } = poolConfig as ICommonConfiguration;
+
+      const reserveAssets = await getParamPerNetwork(ReserveAssets, network);
+
+      const addressesProvider = await getLendingPoolAddressesProvider();
+
+      const testHelpers = await getAaveProtocolDataProvider();
+
+      const admin = await addressesProvider.getPoolAdmin();
+      // 
+      if (!reserveAssets) {
+        throw 'Reserve assets is undefined. Check ReserveAssets configuration at config directory';
+      }
+
+      const treasuryAddress = await getTreasuryAddress(poolConfig);
+
+      const incentivesController = '0x6b8c2D54Fb8519DA47be2977E5147Bc1e03e1960';
+
+      const collateralManager = await deployLendingPoolCollateralManager(verify);
+      await waitForTx(
+        await addressesProvider.setLendingPoolCollateralManager(collateralManager.address)
+      );
+
+      const wethAddress = await getWethAddress(poolConfig);
+      const lendingPoolAddress = await addressesProvider.getLendingPool();
+
+      await deployWETHGateway([wethAddress, lendingPoolAddress]);
+      
+      await initReservesByHelper(
+        ReservesConfig,
+        reserveAssets,
+        WNativeSymbol,
+        admin,
+        treasuryAddress,
+        incentivesController,
+        verify
+      );
+      // 
+      
+      await configureReservesByHelper(ReservesConfig, reserveAssets, testHelpers, admin);
+
+      await deployWalletBalancerProvider(verify);
+    } catch (err) {
+      console.error(err);
+      exit(1);
+    }
+  });
